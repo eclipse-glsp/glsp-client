195515c06c4dd3b20c399d301437f7dc8036c361
Philip Langer <planger@eclipsesource.com>
2020-03-31 13:42:37 +0200

    Add auto-complete widget and edit validation support
    https://github.com/eclipse-glsp/glsp/issues/56

---
+++ src/base/actions/edit-validation-actions.ts
@@ -0,0 +1,76 @@
+/********************************************************************************
+ * Copyright (c) 2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import { Action, generateRequestId, RequestAction, ResponseAction } from "sprotty";
+
+export class RequestEditValidationAction implements RequestAction<SetEditValidationResultAction> {
+    static readonly KIND = "requestEditValidation";
+    kind = RequestEditValidationAction.KIND;
+    constructor(
+        public readonly modelElementId: string,
+        public readonly text: string,
+        public readonly requestId: string = generateRequestId()) { }
+}
+
+export class SetEditValidationResultAction implements ResponseAction {
+    static readonly KIND = "setEditValidationResult";
+    kind = SetEditValidationResultAction.KIND;
+    constructor(public readonly status: ValidationStatus,
+        public readonly responseId: string = '',
+        readonly args?: { [key: string]: string | number | boolean }) { }
+}
+
+export function isSetEditValidationResultAction(action: Action): action is SetEditValidationResultAction {
+    return action !== undefined && (action.kind === SetEditValidationResultAction.KIND)
+        && (<SetEditValidationResultAction>action).status !== undefined;
+}
+
+export interface ValidationStatus {
+    readonly severity: ValidationStatus.Severity;
+    readonly message: string;
+    readonly error: ResponseError;
+}
+
+export interface ResponseError {
+    readonly code: number;
+    readonly message: string;
+    readonly data: Object;
+}
+
+export namespace ValidationStatus {
+
+    export enum Severity {
+        FATAL, ERROR, WARNING, INFO, OK, NONE
+    }
+
+    export const NONE: ValidationStatus = {
+        severity: Severity.NONE, message: '', error: { code: -1, message: '', data: {} }
+    };
+
+    export function isOk(validationStatus: ValidationStatus): boolean {
+        return validationStatus.severity === Severity.OK
+            || validationStatus.severity === Severity.INFO
+            || validationStatus.severity === Severity.NONE;
+    }
+
+    export function isWarning(validationStatus: ValidationStatus): boolean {
+        return validationStatus.severity === Severity.WARNING;
+    }
+
+    export function isError(validationStatus: ValidationStatus): boolean {
+        return validationStatus.severity === Severity.ERROR
+            || validationStatus.severity === Severity.FATAL;
+    }
+}
---
+++ src/base/auto-complete/validation-decorator.ts
@@ -0,0 +1,111 @@
+/********************************************************************************
+ * Copyright (c) 2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import { ValidationStatus } from "../actions/edit-validation-actions";
+
+export interface IValidationDecorator {
+    decorateValidationResult(status: ValidationStatus): void;
+    isValidatedOk(): boolean;
+    dispose(): void;
+}
+
+export namespace IValidationDecorator {
+    export const NO_DECORATION: IValidationDecorator = {
+        decorateValidationResult(status: ValidationStatus) { },
+        isValidatedOk(): boolean { return false; },
+        dispose() { }
+    };
+}
+
+export class ValidationDecorator implements IValidationDecorator {
+
+    warningClasses = ['warning'];
+    warningIconClasses = ['fa', 'fa-question-circle'];
+    errorClasses = ['error'];
+    errorIconClasses = ['fa', 'fa-exclamation-circle'];
+
+    isValidated: boolean = false;
+    hasValidationError: boolean = false;
+
+    protected decorationDiv?: HTMLDivElement;
+
+    constructor(protected containerElement: HTMLElement) { }
+
+    decorateValidationResult(status: ValidationStatus): void {
+        if (ValidationStatus.isError(status)) {
+            this.hasValidationError = true;
+            this.decorateError(status.message ? status.message : 'Error');
+        } else if (ValidationStatus.isWarning(status)) {
+            this.hasValidationError = false;
+            this.decorateWarning(status.message ? status.message : 'Warning');
+        } else {
+            this.hasValidationError = false;
+            this.dispose();
+        }
+        this.isValidated = true;
+    }
+
+    protected decorateError(message: string): void {
+        this.containerElement.classList.add(...this.errorClasses);
+        const div = this.createDecorationDiv();
+        div.classList.add(...this.errorClasses);
+        div.innerHTML = `<span class="${this.errorIconClasses.join(' ')}"></span> ${message}`;
+        this.adjustPosition();
+    }
+
+    protected decorateWarning(message: string): void {
+        this.containerElement.classList.add(...this.warningClasses);
+        const div = this.createDecorationDiv();
+        div.classList.add(...this.warningClasses);
+        div.innerHTML = `<span class="${this.warningIconClasses.join(' ')}"></span> ${message}`;
+        this.adjustPosition();
+    }
+
+    protected createDecorationDiv(): HTMLDivElement {
+        if (!this.decorationDiv) {
+            this.containerElement.classList.add('validation');
+            this.decorationDiv = document.createElement('div');
+            this.decorationDiv.style.width = `${this.decorationContainerWidth()}px`;
+            this.decorationDiv.classList.add('validation-decorator');
+            this.containerElement.appendChild(this.decorationDiv);
+        }
+        return this.decorationDiv;
+    }
+
+    protected decorationContainerWidth(): number {
+        return this.containerElement.clientWidth - 22;
+    }
+
+    protected adjustPosition(): void {
+        if (this.decorationDiv) {
+            const height = this.decorationDiv.clientHeight + 2;
+            this.decorationDiv.style.top = `-${height}px`;
+        }
+    }
+
+    isValidatedOk(): boolean {
+        return this.isValidated && !this.hasValidationError;
+    }
+
+    dispose(): void {
+        this.hasValidationError = false;
+        this.isValidated = false;
+        if (this.decorationDiv && this.containerElement && this.containerElement.contains(this.decorationDiv)) {
+            this.containerElement.removeChild(this.decorationDiv);
+            this.containerElement.classList.remove(...this.errorClasses, ...this.warningClasses);
+            this.decorationDiv = undefined;
+        }
+    }
+}
