69512d75e5088320f6977fd00d84e69d9652dee4
Martin Fleck <mfleck@eclipsesource.com>
2020-04-01 09:47:18 +0200

    Provide test cases for selection-service and adapt where necessary
    - Fixes eclipse-glsp/glsp/issues/44

--- src/features/select/selection-service.ts
+++ src/features/select/selection-service.ts
@@ -52,31 +52,48 @@ export class SelectionService implements SModelRootListener {
         if (root === undefined && select.length === 0 && deselect.length === 0) {
             return;
         }
+        const prevRoot = this.root;
+        const prevSelectedElementIDs = new Set(this.selectedElementIDs);
+
         // update root
         this.root = root;
 
         // update selected element IDs and collect deselected elements
-        const deselected: string[] = [...this.selectedElementIDs].filter(selected => deselect.indexOf(selected) !== -1);
-        for (const id of deselected) {
+        // - select all elements that are not deselected at the same time (no-op)
+        // - deselect all elements that are not selected at the same time (no-op) but was selected
+        const toSelect = [...select].filter(selectId => deselect.indexOf(selectId) === -1);
+        const toDeselect = [...deselect].filter(deselectId => select.indexOf(deselectId) === -1 && this.selectedElementIDs.has(deselectId));
+        for (const id of toDeselect) {
             this.selectedElementIDs.delete(id);
         }
-        for (const id of select) {
+        for (const id of toSelect) {
             this.selectedElementIDs.add(id);
         }
 
+        const deselectedElementIDs = new Set(toDeselect);
         // see if selected elements still exist in the updated root
         for (const id of this.selectedElementIDs) {
             const element = root.index.getById(id);
             if (element === undefined) {
                 this.selectedElementIDs.delete(id);
+                if (prevRoot !== undefined && prevRoot.index.getById(id)) {
+                    deselectedElementIDs.add(id);
+                }
             }
         }
 
-        // aggregate to feedback action handling all elements as only the last feedback is restored
-        this.dispatchFeedback([new SelectFeedbackAction([...this.selectedElementIDs], deselected)]);
+        // only send out changes if there actually are changes, i.e., the root or the selected elements changed
+        const selectionChanged = prevSelectedElementIDs.size !== this.selectedElementIDs.size || ![...prevSelectedElementIDs].every(value => this.selectedElementIDs.has(value));
+        if (selectionChanged) {
+            // aggregate to feedback action handling all elements as only the last feedback is restored
+            this.dispatchFeedback([new SelectFeedbackAction([...this.selectedElementIDs], [...deselectedElementIDs])]);
+        }
 
-        // notify listeners after the feedback action
-        this.notifyListeners(this.root, this.selectedElementIDs);
+        const rootChanged = prevRoot !== root;
+        if (rootChanged || selectionChanged) {
+            // notify listeners after the feedback action
+            this.notifyListeners(this.root, this.selectedElementIDs);
+        }
     }
 
     dispatchFeedback(actions: Action[]) {
