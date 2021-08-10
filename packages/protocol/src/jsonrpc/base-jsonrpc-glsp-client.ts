884f860bf2732ca9551ab6ff34647c249c224262
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-08-10 13:28:21 +0200

    #258 Remove name property from GLSPClient (#130)
    A client is already uniquely identified via its id, there is no need for an additional name property. In our current implementing projects the name property is always equal to the id and therefore redundant.
    
    Part of eclipse-glsp/glsp/issues/258

--- packages/protocol/src/jsonrpc/base-jsonrpc-glsp-client.ts
+++ packages/protocol/src/jsonrpc/base-jsonrpc-glsp-client.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2019-2020 EclipseSource and others.
+ * Copyright (c) 2019-2021 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -21,7 +21,6 @@ import { ConnectionProvider, JsonrpcGLSPClient } from './glsp-jsonrpc-client';
 
 export class BaseJsonrpcGLSPClient implements GLSPClient {
 
-    readonly name: string;
     readonly id: string;
     protected readonly connectionProvider: ConnectionProvider;
     protected connectionPromise?: Promise<MessageConnection>;
