f387c5cbc26b925a785e389705ed6bc048a9d89f
Tobias Ortmayr <tortmayr@eclipsesource.com>
2023-03-17 16:04:56 +0100

    GLSP-954 Ensure that bindAsAService also work with the DI container as context
    The `bindAsService` method failed when the DI container was used as context.
    To avoid this we have to make sure that the bind function receives the correct `this` arg.
    
    Use utility functions in standalone di config

--- examples/workflow-standalone/src/di.config.ts
+++ examples/workflow-standalone/src/di.config.ts
@@ -14,16 +14,16 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 import { createWorkflowDiagramContainer } from '@eclipse-glsp-examples/workflow-glsp';
-import { ConsoleLogger, GLSPDiagramServer, LogLevel, TYPES } from '@eclipse-glsp/client';
+import { bindAsService, bindOrRebind, ConsoleLogger, GLSPDiagramServer, LogLevel, TYPES } from '@eclipse-glsp/client';
 import { Container } from 'inversify';
 import '../css/diagram.css';
 
 export default function createContainer(): Container {
     const container = createWorkflowDiagramContainer('sprotty');
-    container.bind(GLSPDiagramServer).toSelf().inSingletonScope();
-    container.bind(TYPES.ModelSource).toService(GLSPDiagramServer);
-    container.rebind(TYPES.ILogger).to(ConsoleLogger).inSingletonScope();
-    container.rebind(TYPES.LogLevel).toConstantValue(LogLevel.warn);
+    bindAsService(container, TYPES.ModelSource, GLSPDiagramServer);
+    bindOrRebind(container, TYPES.ModelSource).toService(GLSPDiagramServer);
+    bindOrRebind(container, TYPES.ILogger).to(ConsoleLogger).inSingletonScope();
+    bindOrRebind(container, TYPES.LogLevel).toConstantValue(LogLevel.warn);
     container.bind(TYPES.IMarqueeBehavior).toConstantValue({ entireEdge: true, entireElement: true });
     return container;
 }
