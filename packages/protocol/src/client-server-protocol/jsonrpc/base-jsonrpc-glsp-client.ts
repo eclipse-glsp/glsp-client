f97984fea2bcb8e35f9dd4d6aebefd6a6cd9ba6d
Martin Fleck <mfleck@eclipsesource.com>
2024-07-25 14:30:33 +0200

    Ensure no inversify-based code lands in the index
    - Ensure JsonrpcClientProxy is injectable in the glsp-sprotty context
    
    Contributed on behalf of Axon Ivy AG
    Part of https://github.com/eclipse-glsp/glsp/issues/1381

--- packages/protocol/src/client-server-protocol/jsonrpc/base-jsonrpc-glsp-client.ts
+++ packages/protocol/src/client-server-protocol/jsonrpc/base-jsonrpc-glsp-client.ts
@@ -13,7 +13,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import { injectable } from 'inversify';
 import { Disposable, Message, MessageConnection } from 'vscode-jsonrpc';
 import { ActionMessage } from '../../action-protocol/base-protocol';
 import { Emitter, Event } from '../../utils/event';
@@ -175,7 +174,6 @@ export class BaseJsonrpcGLSPClient implements GLSPClient {
 /**
  * Default {@link GLSPClientProxy} implementation for jsonrpc-based client-server communication with typescript based servers.
  */
-@injectable()
 export class JsonrpcClientProxy implements GLSPClientProxy {
     protected clientConnection?: MessageConnection;
     protected enableLogging: boolean;
