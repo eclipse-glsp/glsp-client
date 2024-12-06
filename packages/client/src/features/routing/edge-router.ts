e7f417167908d8249a53e16231cb6a81d94c9fbb
Tobias Ortmayr <tortmayr@eclipsesource.com>
2024-12-06 11:18:19 +0100

    Remove workaround in GLPSManhattanEdgeRouter
    Remove the override of `applyInnerHandleMoves` in `GLSPManhattanEdgeRouter`.
    This workaround was introduced as a fix for https://github.com/eclipse-glsp/glsp-client/pull/200
    but has since then also been fixed in base sprotty: https://github.com/eclipse-sprotty/sprotty/issues/310
    therefore overriding is no longer necessary.

--- packages/client/src/features/routing/edge-router.ts
+++ packages/client/src/features/routing/edge-router.ts
@@ -22,9 +22,7 @@ import {
     GRoutableElement,
     ManhattanEdgeRouter,
     Point,
-    PolylineEdgeRouter,
-    ResolvedHandleMove,
-    almostEquals
+    PolylineEdgeRouter
 } from '@eclipse-glsp/sprotty';
 import { injectable } from 'inversify';
 
@@ -84,46 +82,6 @@ export class GLSPManhattanEdgeRouter extends ManhattanEdgeRouter {
         // users may define all kinds of anchors and anchor computers, we want to make sure we return a valid one in any case
         const anchor = super.getTranslatedAnchor(connectable, refPoint, refContainer, edge, anchorCorrection);
         return Point.isValid(anchor) ? anchor : refPoint;
-    }
-
-    protected override applyInnerHandleMoves(edge: GRoutableElement, moves: ResolvedHandleMove[]): void {
-        const route = this.route(edge);
-        const routingPoints = edge.routingPoints;
-        const minimalPointDistance = this.getOptions(edge).minimalPointDistance;
-        moves.forEach(move => {
-            const handle = move.handle;
-            const index = handle.pointIndex;
-            const correctedX = this.correctX(routingPoints, index, move.toPosition.x, minimalPointDistance);
-            const correctedY = this.correctY(routingPoints, index, move.toPosition.y, minimalPointDistance);
-            switch (handle.kind) {
-                case 'manhattan-50%':
-                    if (index < 0) {
-                        if (routingPoints.length === 0) {
-                            routingPoints.push({ x: correctedX, y: correctedY });
-                            move.handle.pointIndex = 0;
-                        } else if (almostEquals(route[0].x, route[1].x)) {
-                            this.alignX(routingPoints, 0, correctedX);
-                        } else {
-                            this.alignY(routingPoints, 0, correctedY);
-                        }
-                    } else if (index < routingPoints.length - 1) {
-                        if (almostEquals(routingPoints[index].x, routingPoints[index + 1].x)) {
-                            this.alignX(routingPoints, index, correctedX);
-                            this.alignX(routingPoints, index + 1, correctedX);
-                        } else {
-                            this.alignY(routingPoints, index, correctedY);
-                            this.alignY(routingPoints, index + 1, correctedY);
-                        }
-                    } else {
-                        if (almostEquals(route[route.length - 2].x, route[route.length - 1].x)) {
-                            this.alignX(routingPoints, routingPoints.length - 1, correctedX);
-                        } else {
-                            this.alignY(routingPoints, routingPoints.length - 1, correctedY);
-                        }
-                    }
-                    break;
-            }
-        });
     }
 
     override cleanupRoutingPoints(edge: GRoutableElement, routingPoints: Point[], updateHandles: boolean, addRoutingPoints: boolean): void {
