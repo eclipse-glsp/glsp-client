4f5f70b475675ab9eabb1a209df3c9a38b13a9da
Tobias Ortmayr <tortmayr@eclipsesource.com>
2025-09-06 16:19:00 +0200

    Update download script
    Update the download script for the example server.
    - Remove static config via config file (removes overhead of updating the
    file for releases)
    - Introduce optional version parameter, if not specificed the version
    wil be derived from the package.json
    - Remove dependency to shell.js and use node builtins instead

--- examples/workflow-standalone/scripts/start-example-server.ts
+++ examples/workflow-standalone/scripts/start-example-server.ts
@@ -13,33 +13,36 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
+import { execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as sh from 'shelljs';
 import { extract } from 'tar';
-import * as config from './config.json';
-const serverDirPath = path.resolve(__dirname, '..', 'server');
+import * as packageJson from '../../../package.json';
+
+const SERVER_DIR_PATH = path.resolve(__dirname, '..', 'server');
+const FILE_NAME = 'wf-glsp-server-node';
 
 async function run() {
-    const serverFile = await downloadIfNecessary();
-    console.log();
-
     const port = parsePortArg();
     const host = parseHostArg();
+    const version = parseVersion();
+    process.chdir(SERVER_DIR_PATH);
 
-    sh.cd(serverDirPath);
-    sh.exec(`node ${serverFile} -w --port ${port} --host ${host}`);
+    const serverFile = await downloadIfNecessary(version);
+    console.log();
+
+    execSync(`node ${serverFile} -w --port ${port} --host ${host}`, { stdio: 'inherit' });
 }
 
-async function downloadIfNecessary(): Promise<string> {
-    console.log(`Check if server executable with version ${config.version} is present.`);
+async function downloadIfNecessary(version: string): Promise<string> {
+    console.log(`Check if server executable with version ${version} is present.`);
 
-    const existingServer = fs.readdirSync(serverDirPath).find(file => file.startsWith(config.fileName));
+    const existingServer = fs.readdirSync(SERVER_DIR_PATH).find(file => file.startsWith(FILE_NAME));
     if (existingServer) {
-        const existingVersion = existingServer.replace(config.fileName + '-', '').replace('.js', '');
-        const latestVersion = sh
-            .exec(`npm show @eclipse-glsp-examples/workflow-server-bundled@${config.version} version`, { silent: true })
-            .stdout.trim();
+        const existingVersion = existingServer.replace(FILE_NAME + '-', '').replace('.js', '');
+        const latestVersion = execSync(`npm show @eclipse-glsp-examples/workflow-server-bundled@${version} version`, {
+            encoding: 'utf-8'
+        }).trim();
         if (existingVersion === latestVersion) {
             console.log('Server executable already present. Skip download"');
             return existingServer;
@@ -48,32 +51,31 @@ async function downloadIfNecessary(): Promise<string> {
 
     console.log('Server executable with correct version not found.  Download from npm.');
     if (existingServer) {
-        fs.rmSync(path.resolve(serverDirPath, existingServer));
-        fs.rmSync(path.resolve(serverDirPath, existingServer.replace('.js', '.js.map')));
+        fs.rmSync(path.resolve(SERVER_DIR_PATH, existingServer));
+        fs.rmSync(path.resolve(SERVER_DIR_PATH, existingServer.replace('.js', '.js.map')));
     }
-    sh.cd(serverDirPath);
-    const packResultJson = sh
-        .exec(`npm pack @eclipse-glsp-examples/workflow-server-bundled@${config.version} --json`, { silent: true })
-        .stdout.trim();
-    const version = JSON.parse(packResultJson)[0].version;
-    const tarBall = fs.readdirSync(serverDirPath).find(file => file.endsWith('.tar.gz') || file.endsWith('.tgz'))!;
+    const packResultJson = execSync(`npm pack @eclipse-glsp-examples/workflow-server-bundled@${version} --json`, {
+        encoding: 'utf-8'
+    }).trim();
+    const newVersion = JSON.parse(packResultJson)[0].version;
+    const tarBall = fs.readdirSync(SERVER_DIR_PATH).find(file => file.endsWith('.tar.gz') || file.endsWith('.tgz'))!;
     console.log('Extract downloaded server tarball');
     await extract({
         file: tarBall,
-        cwd: serverDirPath
+        cwd: SERVER_DIR_PATH
     });
 
-    const tempDir = path.resolve(serverDirPath, 'package');
-    fs.copyFileSync(path.resolve(tempDir, 'wf-glsp-server-node.js'), path.resolve(serverDirPath, `${config.fileName}-${version}.js`));
+    const tempDir = path.resolve(SERVER_DIR_PATH, 'package');
+    fs.copyFileSync(path.resolve(tempDir, 'wf-glsp-server-node.js'), path.resolve(SERVER_DIR_PATH, `${FILE_NAME}-${newVersion}.js`));
     fs.copyFileSync(
         path.resolve(tempDir, 'wf-glsp-server-node.js.map'),
-        path.resolve(serverDirPath, `${config.fileName}-${version}.js.map`)
+        path.resolve(SERVER_DIR_PATH, `${FILE_NAME}-${newVersion}.js.map`)
     );
 
     console.log('Remove temporary files');
     fs.rmSync(tempDir, { force: true, recursive: true });
-    fs.rmSync(path.resolve(serverDirPath, tarBall), { force: true });
-    return `${config.fileName}-${version}.js`;
+    fs.rmSync(path.resolve(SERVER_DIR_PATH, tarBall), { force: true });
+    return `${FILE_NAME}-${newVersion}.js`;
 }
 
 function parsePortArg(): number {
@@ -82,7 +84,7 @@ function parsePortArg(): number {
     if (portIndex >= 0) {
         port = parseInt(process.argv[portIndex + 1]);
     }
-    if(isNaN(port)) {
+    if (isNaN(port)) {
         console.error('Invalid port number');
         process.exit(1);
     }
@@ -92,14 +94,23 @@ function parsePortArg(): number {
 function parseHostArg(): string {
     let host = 'localhost';
     const hostIndex = process.argv.indexOf('--host');
-    if(hostIndex >= 0) {
+    if (hostIndex >= 0) {
         host = process.argv[hostIndex + 1];
     }
-    if(typeof host !== 'string') {
+    if (typeof host !== 'string') {
         console.error('Invalid host');
         process.exit(1);
     }
     return host;
+}
+
+function parseVersion(): string {
+    const versionIndex = process.argv.indexOf('--version');
+    if (versionIndex >= 0) {
+        return process.argv[versionIndex + 1];
+    }
+    // If no version is specified, use the version of the current package
+    return packageJson.version.endsWith('-next') ? 'next' : packageJson.version;
 }
 
 run();
